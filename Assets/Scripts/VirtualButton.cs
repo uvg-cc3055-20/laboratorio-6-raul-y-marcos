b3282c280010b602907271078ead2dc38b150a88
Alejandro-PC\Alejandro <mon17014@uvg.edu.gt>
2018-02-21 11:30:21 -0600

    Commit
    Todo completado hasta el paso 4

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+//Raul Monzon y Marcos Gutierrez
+
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{   
+   
+    private VirtualButtonBehaviour virtualBtn;
+    public Animator birdAnim;
+
+    // Use this for initialization
+    void Start()
+    {
+        virtualBtn = GetComponent<VirtualButtonBehaviour>();
+        virtualBtn.RegisterEventHandler(this);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+        birdAnim.SetTrigger("sing");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+    }
+}
